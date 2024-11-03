f8334b965c641fe652af6a3f6da83f5ef3d2802b
Tsuji Yoshifumi <tsujiyoshifumi@TsujinoMacBook-Air.local>
2024-11-03 22:56:25 +0900

    feat: implement real time data base

--- src/hooks/useMessage.ts
+++ src/hooks/useMessage.ts
@@ -9,23 +9,39 @@ import {
 import { BASE_PROMPT, OPEN_AI_API_KEY, OPEN_AI_API_URL } from "@/constants";
 import axios from "axios";
 import { db } from "#/firebaseConfig";
-import { query, collection, orderBy, getDocs } from "firebase/firestore";
+import { query, collection, orderBy, getDocs, onSnapshot } from "firebase/firestore";
+import { useState, useEffect } from "react";
 
 export const useMessage = () => {
   const methods = useForm<ChatInputType>();
   const queryClient = useQueryClient();
   const question = queryClient.getQueryData<QuestionType>(["select-quesiton"]);
 
-  const getMessage = async () => {
-    const q = query(collection(db, "message"), orderBy("createdAt"));
-    const querySnapshot = await getDocs(q);
-    return querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-  };
+  // const getMessage = async () => {
+  //   const q = query(collection(db, "message"), orderBy("createdAt"));
+  //   const querySnapshot = await getDocs(q);
+  //   return querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+  // };
 
-  const messages = useQuery<any>({
-    queryKey: ["get-messsage"],
-    queryFn: getMessage,
-  });
+  // const messages = useQuery<any>({
+  //   queryKey: ["get-messsage"],
+  //   queryFn: getMessage,
+  // });
+
+  const [messages, setMessages] = useState<any[]>([]);
+
+  useEffect(() => {
+    const q = query(collection(db, "message"), orderBy("createdAt"));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const newMessages = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setMessages(newMessages);
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   const postMessage = async (data: ChatMessageType) => {
     await axios.post(`/api/firebase`, data);
@@ -95,7 +111,7 @@ ${text}
 
   return {
     methods,
-    messages: messages.data,
+    messages,
     question,
     onSubmit,
   };
